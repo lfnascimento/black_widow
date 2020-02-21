99175f792a021be5ff8d56ad428cbf611acf498e
Luis Fernando Nascimento <nascimento.lsfo@gmail.com>
2020-02-20 22:07:29 -0300

    Add financial transaction create service

---
+++ app/services/financial_transactions/create_service.rb
@@ -0,0 +1,26 @@
+module FinancialTransactions
+  class CreateService
+    attr_reader :source_account, :destination_account, :amount
+
+    def initialize(source_account, destination_account, amount)
+      @source_account = source_account
+      @destination_account = destination_account
+      @amount = amount
+    end
+
+    def perform
+      financial_transaction.source_account.balance -= amount
+      financial_transaction.destination_account.balance += amount
+      financial_transaction.tap(&:save)
+    end
+
+    private
+
+    def financial_transaction
+      @financial_transaction ||= FinancialTransaction.
+        new(source_account: source_account,
+            destination_account: destination_account,
+            amount: amount)
+    end
+  end
+end
